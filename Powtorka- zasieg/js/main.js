698e6d45be06f94b5c882c96a5609d738a2daaf4
Ola Goźlińska <aleksandra.gozlinska@wp.pl>
2018-06-23 11:00:13 +0200

    commit

---
+++ Powtorka- zasieg/js/main.js
@@ -0,0 +1,8 @@
+var zmiennaGlobalna = "AAAAAAAAAAAAAAAAAAAAA";
+
+function zakresWidocznosciZmiennych () {
+   var zmiennaGlobalna = "BBBBBBBBBBBBBBBBBBBB";
+    console.log(zmiennaGlobalna);
+}
+zakresWidocznosciZmiennych();
+console.log(zmiennaGlobalna);
